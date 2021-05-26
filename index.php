10bb8e795e528e567ce2a07f030b18ea7f28ccc6
chantalbenson <chantal.benson99@gmail.com>
2021-05-26 17:43:25 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,130 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<section class="container-fluid"> <!-- container-fluid -->
+
+<div class="container testimoniescontainer"><!-- authenticconnections container -->
+
+    <div class="col-md-6">
+ <h2 class="authenticconnectionsh2"> Authentic Connections</h2>
+        <h2 class="genuinerelationshipsh2">Build Genuine Relationships</h2>
+        
+        <a href="services.html" class="btn btn-testimonies">More Testimonies </a>
+        
+    <img class="img-responsive testimoniesgraphic" alt=“LetsExploreFremantleTogether” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/explore_together.png" >
+   </div> 
+    
+    <div class="col-md-6 testimonyprofilecontainer ">
+    <img class="img-responsive janeparkinprofile" alt=“JaneParkins” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/
+images/testimony-jp.png" width="150">
+    <p class="testimony"> Jane Parkins</p> 
+         <img class="img-responsive speechmarks" alt="speechmarks" src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/speechmarks.png" width="150">   
+    <p class="text-center"> I attended the Ghostly Tours, and the staff were amazing and made the tour a memorable experience. Would recommend FlipSide to anyone looking to experience Fremantle tours in a unique way!</p>
+        <p class="text-center"><strong> 5/5 </strong> </p>
+    
+    </div> 
+    
+    </div> <!--- authenticconnections container -->
+    
+    </section> <!--- container-fluid -->
+
+<section class="container-fluid explorebg"><!--- container fluid -->
+
+<div class="container"> <!---exploretours container -->
+
+    <div class="col-md-6 image-stack">
+    <div class="image-stack__item image-stack__item--top">
+      <img class="img-responsive" alt=“ArtworkFremantle” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/exploreimage_2.png" width="300">
+    </div>
+    <div class="image-stack__item image-stack__item--bottom">
+     <img class="img-responsive" alt=“FremantleCafeStrip” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/exploreimage_1.png" width="600">
+    </div>
+    <div class="image-stack__item--topright">
+      <img class="img-responsive" alt=“FremantleTunnel” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/exploreimage_3.png" width="600">
+    </div>
+       </div> 
+    
+    <div class="col-md-6">
+        <h2 class="exploretoursh2"> Explore Our Fun &amp; </h2>
+        <h2 class="distinctivetoursh2"> Distinctive Tours</h2>
+        <p class="text-center"> From <strong> Ghostly Tours </strong> acting as real ghost hunters exploring haunted places to <strong> Evening Food Tours </strong> to give you a feel and taste of our city, we have something for everyone!</p>
+        
+        <p class="text-center"> Let's bridge the gap between tourists and Fremantle together!</p>
+        <a href="services.html" class="btn btn-explore">Check Out The Tours!</a>
+        
+    </div> 
+    
+ 
+    </div> <!---exploretours container -->
+
+
+    </section> <!--- container fluid -->
+      
+<section class="container-fluid"><!--- container fluid Fall in Love With Our City -->
+    
+
+<div class="container fallinlovewithoutcity"> <!--- container -->
+
+<h2 class="fallinloveh2"> Fall In Love With Our City </h2>
+
+    <div class="row">
+    
+<div class="col-md-8 missioncontainer">
+
+    <p> <strong class="missiontextcolour"> The mission remains the same!</strong> <br> Provide safe, fun, and unforgettable experiences through superior customer service that creates memories and smiles that last a lifetime.</p>
+   
+    <p class="goalstextpadding"> <strong class="goaltextcolour">Our goal is to share </strong> the awesome places we've discovered, and make sure you fall in love with our city of Fremantle too! </p>
+    
+</div>
+        
+<div class="col-md-4">
+ <img class="missionsandgoals img-responsive" alt=“MissionandGoals” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/goal_mission.png" width="180">
+       <a href="aboutus.html" class="btn btn-fallinlovewithourcity">Learn More About Us!</a>
+
+    
+    </div> 
+        
+        
+    </div> <!--- row -->
+    
+    </div> <!--- container -->
+    
+    </section> <!--- container fluid Fall in Love With Our City -->
+      
+      
+ <section class="container-fluid aboutusbg"><!--- container fluid aboutus-->
+
+<div class="container">
+    <div>
+        <h2 class="text-center aboutush2"> Meet The Funky + Creative Hearts </h2>
+    <h2 class="text-center aboutusbehindh2"> Behind Flipside Tours </h2>
+
+    <div class="row"> <!--- aboutjane -->
+<div class="col-md-6">
+<img class="janeimage img-responsive" alt=“JaneBree_TourGuide” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/aboutus_janebree.png" width="250">
+        </div>
+        
+<div class="col-md-6 text-left janeparagraph">        
+    <h3 class="aboutusjane"> Hi, I'm Jane Bree! 
+        <br> Founder + Guide  </h3>
+    <p> I am a songwriter, barrister, and long time-local, with a long family history in Fremantle. I make "bedroom soul" music which you can find on Spotify. I have spent time backpacking through much of the world, including a year living in Madrid, Spain, where I dreamt up this walking tour company.</p>
+        </div> 
+        
+ </div> <!--- aboutjane -->
+        
+    <div class="row rubyrow"> <!--- aboutruby -->
+<div class="col-md-6 text-left rubyparagraph">
+<h3 class="aboutusruby">Hey, I'm Ruby! 
+        <br> Ghost Hunter+ Guide  </h3>
+    <p> As a performer with a background in theatre, I am also a member of several performance ensembles and have performed in Las Vegas, Melbourne, and beyond. When not on stage, I can be found listening to creepy prodcasts and researching ghost stories.</p>
+        </div>
+        
+<div class="col-md-6">        
+<img class="rubyimage img-responsive" alt=“Ruby_TourGuide” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/aboutus_ruby.png" width="250">   
+        </div> 
+        
+ </div> <!--- aboutruby -->
+    
+    
+    </div> <!--- row -->
+     </div> <!--- container -->
+    </section> <!--- container fluid aboutus--> 
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
