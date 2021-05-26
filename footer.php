741a2fd46f5faff55ec75999e6099d4fbece4dae
chantalbenson <chantal.benson99@gmail.com>
2021-05-26 17:43:30 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,58 @@
+<footer class="container-fluid">  <!--- container fluid footer -->
+    
+    <div class="container footer-content">
+<p> <?php bloginfo('name'); ?></p>
+    <h4 class= "text-center letsbefriendsh4"> Let's Be Friends!</h4> 
+        
+        <div class="row socialmediafooter">
+        
+<img src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/Instagram_footer.png" height="75"  alt=“Instagram”>        
+<img src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/facebook_footer.png" height="70" alt=“Facebook”>    
+<img src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/google_footer.png" width="80" alt=“GooglePlus”>             
+            
+        </div>
+    
+        <div class="row joinform">
+
+	<div class="input-group">
+         <input type="email" class="form-control" placeholder="Please enter your email">
+         <span class="input-group-btn">
+         <button class="btn btn-footer" type="submit">Join!</button>
+         </span>
+          </div>  
+            </div>
+        
+        
+         <div class="row tandcfooter">
+             
+<div class="col-md-3 copyright">
+     <p> © Copyright 2021, Flipside Tours </p>
+    
+     </div>
+             
+ <div class="col-md-6 logo">
+<img src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/logo_footer.png" width="110"  alt=“Instagram”> 
+     </div>
+             
+<div class="col-md-3 tanctext">
+     <p> Terms and Conditions </p>
+    
+     </div>
+            </div>
+
+        
+        <nav class="navbar navbar-inverse navfooter">
+
+      <ul class="nav navbar-nav">
+        <li class="active">
+        <li><a href="aboutus.html">Get In Touch!</a></li>
+      </ul>
+    </nav>
+        
+    </div> <!--- container -->
+ 
+      </footer> <!--- footer -->
+
+  </body>
+</html>
+<?php get_footer(); ?>
