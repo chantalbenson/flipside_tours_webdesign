2d141be5a74a7641df982c04fcdc8625a0c3344e
chantalbenson <chantal.benson99@gmail.com>
2021-06-02 12:48:21 +0800

    Update services_page_template.php

--- services_page_template.php
+++ services_page_template.php
@@ -3,171 +3,107 @@
 Template Name: Services
 */
 ?>
-
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 
 <section class="container-fluid"><!--- container fluid evening expereince tour-->
 
 <div class="container"><!--- container -->
-
-<article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-	
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-<div class="post single-page">
-		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
-
-		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-        </div><!--postcontet-->
-		
-	</div><!--post class-->
-
-   
-<?php endwhile; ?>
-	
-	<?php else: ?>
-	<div class="post">
-		<p>Sorry, no posts found.</p>
-	</div><!--post-->
-	<?php endif; ?>
-	</article><!---->
-
+<div class="col-md-6 text-teft servicescontainer">
+    
+<h2 class="eveningexperiencetourh2 text-left"><?php the_field('titleforeveningtours'); ?></h2>
+<p> <strong><?php the_field('paragraphforeveningtoursheading'); ?></strong></p>
+<p><?php the_field('paragraphforeveningtours'); ?></p>
+ <button type="button" class="btn btn-default">Book Evening Tour Now! </button>
+      </div>
+    
 <div class="col-md-6 image-stack">
     <div class="image-stack__item image-stack__item--topevening">
-      <img class="img-responsive" alt=“ArtworkFremantle” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/
-images/eveningtours_imagery02.png" width="200">
+<img class="img-responsive" alt=“FremantleBar” src="<?php the_field('eveningtoursimage02');?>">
     </div>
     <div class="image-stack__item image-stack__item--bottomevening">
-     <img class="img-responsive" alt=“FremantleCafeStrip” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/
-images/eveningtours_imagery01.png" width="350">
+<img class="img-responsive" alt=“FremantleGrilldBurger” src="<?php the_field('eveningtoursimage01');?>">
     </div>
     
 <div class="image-stack__item--graphicevening">
-      <img class="img-responsive" alt=“FremantleTunnel” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/
-images/eveningtours_graphic.png" width="350">
-    </div>
-    
+<img class="img-responsive" alt=“FremantleGraphic” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/eveningtours_graphic.png" width="350">
+    </div>  
 </div>
     </div>
-      </section><!--- container fluid evening expereince tour-->  
+      </section><!--- container fluid evening expereince tour--> 
 
 <section class="container-fluid ghostoursbg"><!--- container fluid paranormal ghost tour-->
 
 <div class="container"><!--- container -->
 
 <div class="col-md-6">
+<h2 class="text-left ghostlytoursh2"><?php the_field('titleforghostlytours'); ?> </h2>
     
-    <div class="image-stack__item image-stack__item--bottomghost">
-     <img class="img-responsive" alt=“FremantleTunnel” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/freamtletunnel.png" width="400">
+<div class="image-stack__item image-stack__item--bottomghost">
+     <img class="img-responsive" alt=“FremantleTunnel” src="<?php the_field('ghostlytoursimage01');?>">
     </div>
-    
     <div class="image-stack__item image-stack__item--topghost">
-      <img class="img-responsive" alt=“Prison” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/prisoncircle.png" width="300">
-    </div>    
+      <img class="img-responsive" alt=“Prison” src="<?php the_field('ghostlytoursimage02');?>">
     </div>
-    
-    <div class="col-md-6 text-left ghosttourdetail">    
-        <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-	
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-<div class="post single-page">
-		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
-
-		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-        </div><!--postcontet-->
-		
-	</div><!--post class-->
-
-   
-<?php endwhile; ?>
-	
-	<?php else: ?>
-	<div class="post">
-		<p>Sorry, no posts found.</p>
-	</div><!--post-->
-	<?php endif; ?>
-	</article><!---->
-    
+</div>
+<div class="col-md-6 text-left ghosttourdetail">
+<p><?php the_field('paragraphforghostlytours'); ?></p>
 <button type="button" class="btn btn-ghost">Book Ghostly Tour Now! </button>
 
 <img class="img-responsive ghostgraphic" alt=“Graphic” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/realghostgraphic.png" width="200">
     </div>
-    </div> 
-</section>
+    </div>   
+</section><!--- container fluid paranormal ghost tour-->
 
 <section class="container-fluid testimoniescontainer"><!--- container fluid testimonies-->
 
 <div class="container"><!--- container -->
     
-<div class="col-md-6 ">
+<h3 class="text-center heartestimonyh3"><?php the_field('titlefortestimonies'); ?></h3>
+<h3 class="text-center communitytestimonyh3"><?php the_field('subtitlefortestimonies'); ?> </h3>
+    
+<div class="col-md-6">
         
- <img class="img-responsive janeparkinprofile" alt=“JaneParkins” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/testimony-jp.png" width="150 ">
-    <p class="testimony"> Jane Parkins</p> 
-         <img class="img-responsive speechmarks" alt="speechmarks" src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/speechmarks.png" width="150 ">   
-   <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-	
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-<div class="post single-page">
-		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
-
-		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-        </div><!--postcontet-->
-		
-	</div><!--post class-->
-
-   
-<?php endwhile; ?>
-	
-	<?php else: ?>
-	<div class="post">
-		<p>Sorry, no posts found.</p>
-	</div><!--post-->
-	<?php endif; ?>
-	</article><!---->
+<img class="img-responsive janeparkinprofile" alt=“JaneParkins” src="<?php the_field('testimoniesimage01');?>">
+<p class="testimony"><?php the_field('testimoniesname01'); ?></p> 
+<img class="img-responsive speechmarks" alt="speechmarks" src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/speechmarks.png" width="150">   
+<p class="text-center"><?php the_field('testimoniesparagraph01'); ?></p>
+<p class="text-center"><strong><?php the_field('testimoniesrating01'); ?></strong></p>
         </div>
     
-    <div class="col-md-6 marcuscontainer">    
+<div class="col-md-6 marcuscontainer">    
 
-        <img class="img-responsive marcussmithprofile" alt=“MarcusSmith” src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/testimonyms.png" width="150">
-    <p class="testimony"> Marcus Smith</p> 
-         <img class="img-responsive speechmarks" alt="speechmarks" src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/speechmark.png" width="150 ">   
-    <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-	
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-<div class="post single-page">
-		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
-
-		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-        </div><!--postcontet-->
-		
-	</div><!--post class-->
-
-   
-<?php endwhile; ?>
-	
-	<?php else: ?>
-	<div class="post">
-		<p>Sorry, no posts found.</p>
-	</div><!--post-->
-	<?php endif; ?>
-	</article><!---->
+<img class="img-responsive marcussmithprofile" alt=“MarcusSmith” src="<?php the_field('testimoniesimage02');?>">
+<p class="testimony"><?php the_field('testimoniesname02'); ?></p> 
+<img class="img-responsive speechmarks" alt="speechmarks" src="http://206.189.45.97/~mesh4/wp-content/themes/flipside_tours_webdesign/images/speechmark.png" width="150">   
+<p class="text-center"><?php the_field('testimoniesparagraph02'); ?></p>
         </div>
   
     </div>
-      </section>
+</section>
+
+ <section class="container-fluid explorebg"><!--- container fluid testimonies-->
+  
+      <h3 class="text-center furtherquestionsh3"> Further Questions? </h3>
+      <h3 class="text-center furtherquestionsh3"> Want To Have A Chat? </h3>
+
+        
+      <div class="container questionscontainer">
+<?php echo do_shortcode("[wpforms id=1158]"); ?>
+<!--
+
+    <input type="text" id="fname" name="firstname" placeholder="First Name..">
+
+  
+    <input type="text" id="lname" name="lastname" placeholder="Last name..">
+    
+    <input type="text" id="email" name="lastname" placeholder="Email..">
+
+    <textarea id="subject" name="subject" placeholder="Message.." style="height:200px"></textarea>
+          
+     <button type="button" class="btn btn-questions">Submit ✓</button>
+-->
+
+</div>
+      
+</section>
 <?php get_footer(); ?>
